1efc4f20f73141c6fb30595845a23754ef4ef55a
“Vimalate <1902644369@qq.com>
2020-03-11 18:24:26 +0800

    users

---
+++ components/public/header/geo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="m-geo">
+      <i class="el-icon-location">广州</i>
+      <nuxt-link to="/changeCity" class="changeCity">切换城市</nuxt-link>
+      [北京，上海，深圳]
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    };
+  },
+
+}
+
+</script>
+<style lang='scss' scoped>
+</style>
